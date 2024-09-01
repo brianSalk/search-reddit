a2742710ec785bd3681618b5fd868eac27d6e540
BrianSalk <brian.salkas@gmail.com>
2024-09-01 11:42:17 -0500

    Allow user to specify whether to search in comments

--- frontend.py
+++ frontend.py
@@ -3,14 +3,23 @@ import praw_stuff
 
 if __name__ == '__main__':
     st.title('Search Reddit')
-    subreddits = subreddits = st.text_input('subreddits').split()
+    subreddits =    st.text_input('subreddits').split()
     search_string = st.text_input('word or phrase to search')
+
     is_case_sensitive = st.checkbox('case sensitive search')
     is_whole_word = st.checkbox('search as whole word')
     is_regex = st.checkbox('use extended regex')
-    include_comments = st.checkbox('include comments', value=True)
+    include_comments = st.checkbox('search in comments', value=True)
+    st.write(include_comments)
 
 
-    if st.button('search subreddit'):
-        praw_stuff.search_in_subreddits(search_string, subreddits, not is_case_sensitive, is_whole_word, is_regex, include_comments )
-    # st.button('search subreddits', on_click=praw_stuff.search_in_subreddits, args=(search_string, subreddits, not is_case_sensitive, is_whole_word, is_regex, include_comments ))
+    if st.button('search subreddit') or search_string:
+        with st.spinner(f'finding occurances of {search_string} in {" ".join(subreddits)}...'):
+            praw_stuff.search_in_subreddits(
+                    search_string, 
+                    subreddits, 
+                    ignore_case = not is_case_sensitive, 
+                    whole_word=is_whole_word, 
+                    use_regex=is_regex, 
+                    include_comments=include_comments 
+                    )
--- praw_stuff.py
+++ praw_stuff.py
@@ -15,7 +15,7 @@ def get_bad_subreddits(subreddits):
     nonexistant = []
     for subreddit in subreddits:
         try:
-            reddit.subreddits.search_by_name(subreddit, exact=True)
+            reddit.subreddit(subreddit).new(limit=None)
         except Exception as e:
             nonexistant.append(subreddit)
     return nonexistant
@@ -28,62 +28,69 @@ def squeeze_spaces(s):
 
 
 def find_string_in_subreddit(string, subreddit_name, ignore_case=True, whole_word=False, use_regex=False, include_comments=True):
-    bad_subs = get_bad_subreddits([subreddit_name]) 
-    print('BAD SUBS _______________ ', bad_subs)
-    if bad_subs:
-        print(bad_subs)
-        return
     # get each submission within the subreddit
-    for submission in reddit.subreddit(subreddit_name).new(limit=None):
-        found_in_post = False
-        found_in_comments_count = 0
-        url = submission.url 
-        title = submission.title
-        selftext = submission.selftext
+    try:
+        for submission in reddit.subreddit(subreddit_name).new(limit=None):
+            found_in_post = False
+            found_in_comments_count = 0
+            url = submission.url 
+            title = submission.title
+            selftext = submission.selftext
 
-        title = squeeze_spaces(title)
-        selftext = squeeze_spaces(selftext)
-        if ignore_case:
-            title = title.lower()
-            selftext = selftext.lower()
-            string = string.lower()
-        print('submission: ', title)
-        if string in title or string in selftext:
-            found_in_post = True
-        comment_forest = None
-        # SEARCH COMMENTS
-        submission.comments.replace_more(limit=None)
-        for comment in submission.comments.list():
-            print('comment')
-            comment_body = comment.body
-            comment_body = squeeze_spaces(comment_body)
+            title = squeeze_spaces(title)
+            selftext = squeeze_spaces(selftext)
             if ignore_case:
-                comment_body = comment_body.lower()
-            if string in comment_body:
-                found_in_comments_count += 1
-        comment = ''
-        if found_in_comments_count == 1:
-            comment = f'found in {found_in_comments_count} comment'
-        if found_in_comments_count > 1:
-            comment = f'found in {found_in_comments_count} comments'
+                title = title.lower()
+                selftext = selftext.lower()
+                string = string.lower()
+            print('submission: ', title)
+            if string in title or string in selftext:
+                found_in_post = True
+            comment_forest = None
+            # SEARCH COMMENTS
+            if include_comments:
+                submission.comments.replace_more(limit=None)
+                print('comment searched')
+                for comment in submission.comments.list():
+                    print('comment')
+                    comment_body = comment.body
+                    comment_body = squeeze_spaces(comment_body)
+                    if ignore_case:
+                        comment_body = comment_body.lower()
+                    if string in comment_body:
+                        found_in_comments_count += 1
+            comment = ''
+            if found_in_comments_count == 1:
+                comment = f'found in {found_in_comments_count} comment'
+            if found_in_comments_count > 1:
+                comment = f'found in {found_in_comments_count} comments'
 
-        if found_in_post or found_in_comments_count > 1:
-            st.markdown(f'[{title}](%s) {comment}' % url)
+            if found_in_post or found_in_comments_count > 1:
+                st.markdown(f'[{title}](%s) {comment}' % url)
+    except Exception as e:
+        st.write(':red[{subreddit_name} not found, check your spelling]')
 
 
 def search_in_subreddits(string, subreddit_names, ignore_case=True, whole_word=False, use_regex=False, include_comments=True):
     if not string or not subreddit_names:
         st.write(':red[search string and subreddits cannot be blank]')
         return
-    bad_subs = get_bad_subreddits(subreddit_names)
-    if bad_subs:
-        st.write(f'red:[{bad_subs} not found]')
-        return
+    st.spinner('scraping reddit')
     for subreddit_name in subreddit_names:
         st.markdown("<h2 style='color: blue;'>" + f"Searching for '{string}' in:" + "</h2>", unsafe_allow_html=True)
         st.markdown("<h3 style='color: blue;'>" + "r/" + subreddit_name + "</h3>", unsafe_allow_html=True)
         subreddit_name = subreddit_name.lower()
-        find_string_in_subreddit(string, subreddit_name, ignore_case=True, whole_word=False, use_regex=False, include_comments=True)    
+        try:
+            find_string_in_subreddit(
+                    string, 
+                    subreddit_name, 
+                    ignore_case=ignore_case, 
+                    whole_word=whole_word, 
+                    use_regex=use_regex, 
+                    include_comments=include_comments
+                    )    
+        except Exception as e:
+            st.write('red:[One or more subreddits not found]')
 
 
 
