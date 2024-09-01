2567611e58dc37a77ad1e02189ca02f9542468d5
BrianSalk <brian.salkas@gmail.com>
2024-09-01 13:49:12 -0500

    Fix help text for whole word checkbox

--- frontend.py
+++ frontend.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         search_string = st.text_input('word or phrase to search')
 
         is_case_sensitive = st.checkbox('make search case sensitive search')
-        is_whole_word = st.checkbox('search as whole word', help="Check this box to avoid matching for parts of words \nIf you are searching 'ed' but do not want to show matches for 'helped' or 'education'")
+        is_whole_word = st.checkbox('search as whole word', help="Check this box to avoid matching for parts of words.  For example, if you are searching 'ed' but do not want to show matches for 'helped' or 'education'")
         is_regex = st.checkbox('use python regex (programmers only)')
         include_comments = st.checkbox('search for word or phrase in submission comments', value=True)
         submitted = st.form_submit_button('Search reddit')
